67337d550da9cf943132e52bb8bfae9e41370118
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2023-10-21 14:12:35 -0700

    add logic for card component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+// import Card from './Card';
+import CardList from './CardList'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { robots } from './robots';
 
-ReactDOM.render()
+ReactDOM.render(
+  <CardList robots={robots} />
+, document.getElementById('root'))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Card />
+//     {/* <App /> */}
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
